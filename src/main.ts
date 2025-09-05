48d4f58da32c7b700d71d123e48c6f59d9cbda49
Guitar <jilamikalalae@gmail.com>
2025-09-05 22:01:45 +0700

    fix enable cors

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
+  app.enableCors(); 
   await app.listen(process.env.PORT ?? 3000);
 
-  app.enableCors(); 
+  
 }
 bootstrap();
